1f53c4aad1681258d3f74d06fdbb8226aa7f9610
SofiaGracia <sofiagracia000@gmail.com>
2024-02-12 17:17:27 +0100

    3. Actualització dels archius de javadoc

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.sumanth.project","c":"BankingApplication","l":"BankingApplication()","u":"%3Cinit%3E()"},{"p":"com.sumanth.project","c":"BankingApplication","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
